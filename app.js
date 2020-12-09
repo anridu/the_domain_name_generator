9ad442b4d491e430dd45301fd665cdbed3404168
anridu <angelriverod@gmail.com>
2020-12-09 18:52:02 +0000

    añado ejercicio terminado

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
-
-
-    var final = [];
-    for (var i = 0, l = pronoun.length; i < l; i++) {
-        for (var j = 0, k = 0, m = adj.length, n = noun.length; j < m; j++) {
-            final.push([pronoun[i], adj[j], noun[k]]);
-            if (j == m - 1 && k < n - 1) {
-                k++;
-                j = -1;
+let fin = ['.us', '.net']
+for (var i = 0; i < pronoun.length; i++) {
+    for (var j = 0; j < adj.length; j++) {
+        for (var k = 0; k < noun.length; k++) {
+            for (var l = 0; l < fin.length; l++) {
+                var nombrecom2 = pronoun[i] + adj[j] + noun[k] + fin[l];
+                console.log(nombrecom2);
             }
         }
     }
-    
-    console.log(final);
-   
+}
+
